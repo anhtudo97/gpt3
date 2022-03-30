dacf19c8bd33f60dbcc7b90dc8cd1fe85a7d99ee
Zhao Yang <anhtudo97@gmail.com>
2022-03-30 23:21:47 +0700

    feat(update): app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from "react";
 
+import {
+  Header,
+  Footer,
+  Blog,
+  Posibility,
+  Features,
+  WhatGPT3,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+
+import "./App.css";
+
 const App = () => {
-  return <div>GPT3 - Anhtudo97</div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Posibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
